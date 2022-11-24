2ea115c31fd52e5a0d03c34a8192e762ab7778e7
vitalijdegtiar <101144853+vitalijdegtiar@users.noreply.github.com>
2022-11-24 17:13:43 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+url_base = 'https://b2c.passport.rt.ru'
+
+url_reg ='/auth'
+
+positive_email = "madcat78@yandex.ru"
+positive_password = "RT_Parol"
+positive_phone = "79215988544"
+
+
+negative_email = "222madcat78@yandex.ru"
+negative_phone = "75858658565"
