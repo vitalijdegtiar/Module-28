960d66b06538fba9cd033704558f385422a2408e
vitalijdegtiar <101144853+vitalijdegtiar@users.noreply.github.com>
2022-11-29 18:00:05 +0300

    Add files via upload

--- tests/test_auth_rt.py
+++ tests/test_auth_rt.py
@@ -13,25 +13,19 @@ from pages.account_page import AccountHelper
 from pages.agreement_page import AgreementHelper
 
 
-# икстура для составления красивого отчета
-# @allure.story('test')
-# @allure.feature('test ya')
-# @allure.step('test searh')
+
 def test_auth_phone(browser):
     auth_main_page = AuthHelper(browser)
     auth_main_page.go_to_site()
     auth_main_page.click_on_phone_swich()
     assert auth_main_page.mobile_is_visible()
-    # auth_main_page.enter_username("Hello")
-    # auth_main_page.click_on_auth_button()
-    # elements = ya_search_page.check_nave_bar()
-    # assert "Картинки" and "Видео" in elements
+
+
 def test_auth_email(browser):
     auth_main_page = AuthHelper(browser)
     auth_main_page.go_to_site()
     auth_main_page.click_on_email_swich()
     assert auth_main_page.email_is_visible()
-
 
 def test_auth_login(browser):
     auth_main_page = AuthHelper(browser)
@@ -46,7 +40,6 @@ def test_auth_ls(browser):
     auth_main_page.click_on_ls_swich()
     assert auth_main_page.ls_is_visible()
 
-
 def test_forgot_pass(browser):
     auth_main_page = AuthHelper(browser)
     auth_main_page.go_to_site()
@@ -60,7 +53,6 @@ def test_forgot_pass_reset(browser):
     auth_main_page.click_on_reset()
     assert auth_main_page.auth_is_visible()
 
-
 def test_user_agreement_1(browser):
     auth_main_page = AuthHelper(browser)
     agreement_page = AgreementHelper(browser)
@@ -68,6 +60,10 @@ def test_user_agreement_1(browser):
     auth_main_page.click_on_user_agreement_link_1()
     auth_main_page.switch_new()
     assert agreement_page.agreement_is_visible()
+
+def test_close_current_window(browser):
+    auth_main_page = AuthHelper(browser)
+    auth_main_page.close_current_window()
 
 def test_user_agreement_2(browser):
     auth_main_page = AuthHelper(browser)
@@ -77,13 +73,9 @@ def test_user_agreement_2(browser):
     auth_main_page.switch_new()
     assert agreement_page.agreement_is_visible()
 
-
-def test_chat(browser):
+def test_close_current_window_2(browser):
     auth_main_page = AuthHelper(browser)
-    auth_main_page.go_to_site()
-    auth_main_page.click_on_chat_link()
-    assert auth_main_page.chat_is_visible()
-
+    auth_main_page.close_current_window()
 
 def test_viber(browser):
     auth_main_page = AuthHelper(browser)
@@ -91,7 +83,7 @@ def test_viber(browser):
     auth_main_page.click_on_viber_link()
     auth_main_page.switch_new()
     auth_main_page.driver.save_screenshot('viber_page.png')
-
+    auth_main_page.close_current_window()
 
 def test_telega(browser):
     auth_main_page = AuthHelper(browser)
@@ -99,14 +91,8 @@ def test_telega(browser):
     auth_main_page.click_on_telega_link()
     auth_main_page.switch_new()
     auth_main_page.driver.save_screenshot('telega_page.png')
+    auth_main_page.close_current_window()
 
-
-def test_support_phone(browser):
-    auth_main_page = AuthHelper(browser)
-    auth_main_page.go_to_site()
-    auth_main_page.click_on_support_phone_link()
-    time.sleep(5)
-    auth_main_page.driver.save_screenshot('Support_phone.png')
 
 def test_registration(browser):
     auth_main_page = AuthHelper(browser)
@@ -158,6 +144,10 @@ def test_cp(browser):
     auth_main_page.switch_new()
     assert agreement_page.agreement_is_visible()
 
+def test_return_to_wnd_before_3(browser):
+    auth_main_page = AuthHelper(browser)
+    auth_main_page.close_current_window()
+
 def test_positive_auth_email(browser):
     auth_main_page = AuthHelper(browser)
     account_page = AccountHelper(browser)
@@ -168,10 +158,35 @@ def test_positive_auth_email(browser):
     auth_main_page.click_on_checkbox()
     auth_main_page.click_on_auth_button()
     assert account_page.lk_is_visible()
-    time.sleep(5)
+
+def test_logout(browser):
+    auth_main_page = AuthHelper(browser)
+    account_page = AccountHelper(browser)
+    auth_main_page.go_to_site()
     account_page.logout()
     assert auth_main_page.auth_is_visible()
 
+def test_negative_auth_login(browser):
+    auth_main_page = AuthHelper(browser)
+    auth_main_page.go_to_site()
+    auth_main_page.click_on_login_swich()
+    auth_main_page.enter_username(config.negative_login)
+    auth_main_page.enter_password(config.positive_password)
+    auth_main_page.click_on_checkbox()
+    auth_main_page.click_on_auth_button()
+    assert auth_main_page.error_is_visible()
+
+def test_positive_logout(browser):
+    auth_main_page = AuthHelper(browser)
+    account_page = AccountHelper(browser)
+    auth_main_page.go_to_site()
+    auth_main_page.click_on_email_swich()
+    auth_main_page.enter_username(config.positive_email)
+    auth_main_page.enter_password(config.positive_password)
+    auth_main_page.click_on_checkbox()
+    auth_main_page.click_on_auth_button()
+    account_page.logout()
+    assert auth_main_page.auth_is_visible()
 
 
 def test_negative_auth_email(browser):
@@ -194,6 +209,11 @@ def test_positive_auth_phone(browser):
     auth_main_page.click_on_checkbox()
     auth_main_page.click_on_auth_button()
     assert account_page.lk_is_visible()
+
+def test_logout_1(browser):
+    auth_main_page = AuthHelper(browser)
+    account_page = AccountHelper(browser)
+    auth_main_page.go_to_site()
     account_page.logout()
     assert auth_main_page.auth_is_visible()
 
@@ -206,3 +226,39 @@ def test_negative_auth_phone(browser):
     auth_main_page.click_on_checkbox()
     auth_main_page.click_on_auth_button()
     assert auth_main_page.error_is_visible()
+
+def test_negative_auth_ls(browser):
+    auth_main_page = AuthHelper(browser)
+    auth_main_page.go_to_site()
+    auth_main_page.click_on_ls_swich()
+    auth_main_page.enter_username(config.negative_ls)
+    auth_main_page.enter_password(config.positive_password)
+    auth_main_page.click_on_checkbox()
+    auth_main_page.click_on_auth_button()
+    assert auth_main_page.error_is_visible()
+
+def test_chat(browser):
+    auth_main_page = AuthHelper(browser)
+    auth_main_page.go_to_site()
+    auth_main_page.click_on_chat_link()
+    assert auth_main_page.chat_is_visible()
+
+# def test_close_chat(browser):
+#     auth_main_page = AuthHelper(browser)
+#     # auth_main_page.go_to_site()
+#     # auth_main_page.click_on_chat_link()
+#     time.sleep(5)
+#     # auth_main_page.click_on_chat_btn()
+#     time.sleep(5)
+#     auth_main_page.click_on_close_chat_btn()
+#     time.sleep(5)
+#     assert auth_main_page.chat_is_visible()
+#
+# def test_support_phone(browser):
+#     auth_main_page = AuthHelper(browser)
+#     auth_main_page.go_to_site()
+#     auth_main_page.click_on_support_phone_link()
+#     time.sleep(5)
+#     auth_main_page.driver.save_screenshot('Support_phone.png')
+#     browser.escape()
+#     time.sleep(5)
